ed6a50c88cc61eef22c0ba4861325fca7efde97e
luiseufrasio <luis.eufrasio@gmail.com>
2024-06-25 22:58:40 -0300

    simple secured method

--- src/main/java/fish/payara/api/SecuredResource.java
+++ src/main/java/fish/payara/api/SecuredResource.java
@@ -1,9 +1,11 @@
 package fish.payara.api;
 
+import fish.payara.util.JWTUtils;
+import io.jsonwebtoken.Claims;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.HeaderParam;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -13,7 +15,17 @@ public class SecuredResource {
     @GET
     @Path("/hi-with-token")
     @Produces(MediaType.TEXT_PLAIN)
-    public String getUnsecuredHello() {
-        return "Hey! This is an unsecured endpoint.";
+    public Response getSecuredHello(@HeaderParam("Authorization") String authHeader) {
+        if (authHeader == null || authHeader.isEmpty()) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Include a token into the Header")
+                    .build();
+        }
+
+        String token = authHeader.substring("Bearer".length()).trim();
+        Claims claims = JWTUtils.decodeToken(token);
+        String username = claims.getSubject();
+        return Response.ok("Hey, " + username + "! You are connected to a secured endpoint.")
+                .build();
     }
 }
